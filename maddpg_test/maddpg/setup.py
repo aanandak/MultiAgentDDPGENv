e1561a3b53b165953c338e7e48d2c39f6f741afb
aanandak <77517705+aanandak@users.noreply.github.com>
2022-04-16 15:25:28 -0400

    First commit

---
+++ maddpg_test/maddpg/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='maddpg',
+      version='0.0.1',
+      description='Multi-Agent Deep Deterministic Policy Gradient',
+      url='https://github.com/openai/maddpg',
+      author='Igor Mordatch',
+      author_email='mordatch@openai.com',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+            'gym', 
+            'numpy-stl',
+            'tensorflow>=1.8.0,<2.0']
+)
